3c11f9e3621031b9ff20530bb9d1667296acab83
khansuhad <suhadahmodkhan@gmail.com>
2023-08-16 17:35:07 +0600

    fixed an withdraw error

--- js/withdraw.js
+++ js/withdraw.js
@@ -6,9 +6,16 @@ document.getElementById('withdraw-button').addEventListener('click',function(){
     const withdrawInput = Number(document.getElementById('withdraw-input').value);
 
     
+    
+    if(withdrawInput > balanceValue){
+        alert("insufficient Amount ,you can't withdraw.");
+        document.getElementById('withdraw-input').value = '';
+        return ;
+    }
     withdraw.innerText = withdrawValue + withdrawInput ;
     balance.innerText = balanceValue - withdrawInput ;
-
     document.getElementById('withdraw-input').value = '';
+
+    
 
 })
